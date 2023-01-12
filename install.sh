54d3ad4f41912b18d99935287ca567a550f596b2
MarcoWel <mw@welectron.com>
2023-01-12 19:17:03 +0100

    Include optional system update

--- install.sh
+++ install.sh
@@ -2,6 +2,19 @@
 
 echo "INSTALLING BASHTOOLS"
 echo
+
+echo -n "Update system first (apt full-upgrade)? y/N "
+read res
+if [ $res == "y" ]
+then
+    echo "Performing system update..."
+    echo
+    sudo apt update
+    sudo apt -y full-upgrade
+    sudo apt -y autoremove
+fi
+
+echo
 echo "Installing prerequesites..."
 echo
 
